ae3124c1207d7f439186b85fdae09d33d8e4ecc7
Vicente Ramirez <vicente.ramirezg@gmail.com>
2025-02-06 22:07:07 -0500

    Force HTTPS settings

--- miami_racket_club/miami_racket_club/settings.py
+++ miami_racket_club/miami_racket_club/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-SECURE_SSL_REDIRECT = True  # Redirect all non-HTTPS traffic to HTTPS
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
     config('HEROKU_URL'),
