f03e9b356deba182c85ac488afd014ddd5bf25fa
vramirezgalleria <vramirez@galleriafarms.com>
2025-02-08 14:09:09 -0500

    Format survey button.

--- miami_racket_club/rankings/views.py
+++ miami_racket_club/rankings/views.py
@@ -184,7 +184,7 @@ def send_match_notification(match):
                     <p><strong>📝 Notes:</strong> {match.notes}</p>
                     <p style="text-align: center;">
                         <a href="{profile_url}" class="profile-button">View Profile</a>
-                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfSGy5zy6x8zSiVnA5s2a7ImnUOgiP3hN4rbmecO_RYJWRu6Q/viewform?usp=sharing" class="survey-button">Fill Out Survey</a>
+                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfSGy5zy6x8zSiVnA5s2a7ImnUOgiP3hN4rbmecO_RYJWRu6Q/viewform?usp=sharing" class="profile-button">Fill Out Survey</a>
                     </p>
                 </div>
                 <div class="footer">
